49edec02a3fed78f1a23f8f00365e54034bceb34
Garrick Chiu <74120169+GarrickC23@users.noreply.github.com>
2023-01-14 22:07:36 -0800

    Teleportation Added

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public GameObject firstPortal; 
+    public GameObject secondPortal; 
+    public GameObject player; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if ( collision.gameObject == player )
+        {
+            //Debug.Log("working");
+            var temp = new Vector3(secondPortal.transform.position.x, secondPortal.transform.position.y + 1, secondPortal.transform.position.z + 1);
+            player.transform.position = temp; 
+        }
+    }
+}
